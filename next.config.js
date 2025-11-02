c903bb171c22f22f051dbf7ee433d24b6a333510
Dan Leffler <leffleryd@gmail.com>
2025-11-03 09:38:12 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-// FILE: next.config.js
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-images: {
-remotePatterns: [
-{ protocol: 'https', hostname: 'images.unsplash.com' },
-{ protocol: 'https', hostname: 'cdn.shopify.com' }
-]
-}
-}
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'cdn.shopify.com' }
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
+};
+
+module.exports = nextConfig;
