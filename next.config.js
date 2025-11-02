efcc7ddf84fa6f2d0c04a97992eb7c04026ab69a
Dan Leffler <leffleryd@gmail.com>
2025-11-03 09:26:52 +1100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// FILE: next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+images: {
+remotePatterns: [
+{ protocol: 'https', hostname: 'images.unsplash.com' },
+{ protocol: 'https', hostname: 'cdn.shopify.com' }
+]
+}
+}
+module.exports = nextConfig
