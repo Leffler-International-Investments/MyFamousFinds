5fe24f5e4dbc1fc1efe8fcad3a73327632450694
Dan Leffler <leffleryd@gmail.com>
2025-11-03 13:53:28 +1100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-// FILE: pages/_app.tsx
-import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+// FILE: /pages/_app.tsx
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
