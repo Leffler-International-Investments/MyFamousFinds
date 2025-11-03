2d0f03ca7277e68e31041d5592f74bf785dd5590
Dan Leffler <leffleryd@gmail.com>
2025-11-03 14:47:00 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-// FILE: tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-theme: { extend: {} },
-plugins: []
-}
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
