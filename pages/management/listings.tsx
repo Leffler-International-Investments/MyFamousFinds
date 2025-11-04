e6d3a3fda06d9f219dcc84836b837d53d2788e3b
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:23:12 +1100

    Create listings.tsx

---
+++ pages/management/listings.tsx
@@ -0,0 +1,6 @@
+// FILE: /pages/management/listings.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+
+export default function ManagementListings() {
+  return <AdminPlaceholder pageTitle="All Listings" />;
+}
