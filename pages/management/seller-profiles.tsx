322d94700bb0b80dba5221aa5d0bf5c5ae50627d
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:27:09 +1100

    Create seller-profiles.tsx

---
+++ pages/management/seller-profiles.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/seller-profiles.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementSellerProfiles() {
+  return <AdminPlaceholder pageTitle="Seller Profiles / Controls" />;
+}
