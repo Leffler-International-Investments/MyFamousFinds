12553121370e6e7b1bd98af8785f3515a879a91e
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:26:51 +1100

    Create users.tsx

---
+++ pages/management/users.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/users.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementUsers() {
+  return <AdminPlaceholder pageTitle="User & Role Management" />;
+}
