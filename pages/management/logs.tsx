433db0b38fb3422ea8d649502b5140f595e3596d
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:32:03 +1100

    Create logs.tsx

---
+++ pages/management/logs.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/logs.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementLogs() {
+  return <AdminPlaceholder pageTitle="Logs & Audit Trail" />;
+}
