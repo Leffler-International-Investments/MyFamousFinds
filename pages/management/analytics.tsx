e706b7b229163b3d3187a0151880d517681d4f4e
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:30:50 +1100

    Create analytics.tsx

---
+++ pages/management/analytics.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/analytics.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementAnalytics() {
+  return <AdminPlaceholder pageTitle="Analytics & Reports" />;
+}
