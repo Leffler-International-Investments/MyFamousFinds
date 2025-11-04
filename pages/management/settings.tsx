e6a20b2cb6d1263efd71b75d1e5b74f0353b721a
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:31:41 +1100

    Create settings.tsx

---
+++ pages/management/settings.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/settings.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementSettings() {
+  return <AdminPlaceholder pageTitle="System Settings" />;
+}
