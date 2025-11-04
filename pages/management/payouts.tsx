7e1e53cffb5d9665e7f6de9fe9d241edf56d270d
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:29:48 +1100

    Create payouts.tsx

---
+++ pages/management/payouts.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/payouts.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementPayouts() {
+  return <AdminPlaceholder pageTitle="Payouts & Finance" />;
+}
