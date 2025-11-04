973091856ccf2557e682be8277fa12de7272380d
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:29:03 +1100

    Update orders.tsx

--- pages/management/orders.tsx
+++ pages/management/orders.tsx
@@ -1,6 +1,5 @@
 // FILE: /pages/management/orders.tsx
 import AdminPlaceholder from "../../components/AdminPlaceholder";
-
 export default function ManagementOrders() {
   return <AdminPlaceholder pageTitle="Orders Overview" />;
 }
