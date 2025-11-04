0ebf5bee8c61ed1b8ca052247eb4c9d667f1bac2
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:31:15 +1100

    Create support-tickets.tsx

---
+++ pages/management/support-tickets.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/support-tickets.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementSupport() {
+  return <AdminPlaceholder pageTitle="Support Tickets" />;
+}
