36b794e7e3e32310f8ecee8c3e67cae8f5e63c47
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:30:13 +1100

    Create tax.tsx

---
+++ pages/management/tax.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/tax.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementTax() {
+  return <AdminPlaceholder pageTitle="Tax & Compliance" />;
+}
