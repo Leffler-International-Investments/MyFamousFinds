f25d82a45a976fca08b88f8f128e237f49efc058
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:28:40 +1100

    Create content.tsx

---
+++ pages/management/content.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/content.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementContent() {
+  return <AdminPlaceholder pageTitle="Content Management" />;
+}
