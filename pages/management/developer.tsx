16b584549a7e880df47882ae24d4df98d355160c
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:32:24 +1100

    Create developer.tsx

---
+++ pages/management/developer.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/developer.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementDeveloper() {
+  return <AdminPlaceholder pageTitle="Developer / Integrations" />;
+}
