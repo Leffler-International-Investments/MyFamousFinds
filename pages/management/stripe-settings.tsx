fbc86c55e3498f4f5bfe0148394858b2ad46ae19
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:30:31 +1100

    Create stripe-settings.tsx

---
+++ pages/management/stripe-settings.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/stripe-settings.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementStripe() {
+  return <AdminPlaceholder pageTitle="Stripe & Payment Settings" />;
+}
