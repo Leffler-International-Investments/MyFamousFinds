add84a6b4f5c2e0bd01b1a5724f6abbe3060fe6f
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:29:25 +1100

    Create disputes.tsx

---
+++ pages/management/disputes.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/disputes.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementDisputes() {
+  return <AdminPlaceholder pageTitle="Returns & Disputes" />;
+}
