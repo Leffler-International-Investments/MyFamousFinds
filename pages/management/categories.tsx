c2906e41cebd439bc22dea056b3a8ee0fc07063a
Dan Leffler <leffleryd@gmail.com>
2025-11-05 08:27:58 +1100

    Create categories.tsx

---
+++ pages/management/categories.tsx
@@ -0,0 +1,5 @@
+// FILE: /pages/management/categories.tsx
+import AdminPlaceholder from "../../components/AdminPlaceholder";
+export default function ManagementCategories() {
+  return <AdminPlaceholder pageTitle="Categories & Attributes" />;
+}
