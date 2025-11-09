732758bdb918e783ff063d608042e2ef8b30c88e
Dan Leffler <leffleryd@gmail.com>
2025-11-09 18:43:35 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 👇 This line is the important bit for Next 16
+  turbopack: {},
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "cdn.shopify.com" },
     ],
   },
+
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
