7b566940277d579c0cce50910ec8887725a1e9e0
Dan Leffler <leffleryd@gmail.com>
2025-11-09 16:17:33 +1100

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,8 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("STRIPE_SECRET_KEY is not set");
 }
 
-// We let Stripe use its default API version for this SDK.
-// (Do NOT hard-code "2023-10-16" here, that's what caused the TS error before.)
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function createCheckoutSession(
