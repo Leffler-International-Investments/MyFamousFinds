ea20c4286abe7297dc7a2f764238d22ad6d16c14
Dan Leffler <leffleryd@gmail.com>
2025-11-09 16:17:49 +1100

    Update getStripe.ts

--- lib/getStripe.ts
+++ lib/getStripe.ts
@@ -1,17 +1,19 @@
 // FILE: /lib/getStripe.ts
 import { Stripe, loadStripe } from "@stripe/stripe-js";
 
-let stripePromise: Promise<Stripe | null> | null = null;
+let stripePromise: Promise<Stripe | null>;
 
 export function getStripe() {
   if (!stripePromise) {
-    const key = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-    if (!key) {
+    const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+
+    if (!publishableKey) {
       console.error("NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not set");
-      stripePromise = Promise.resolve(null);
-    } else {
-      stripePromise = loadStripe(key);
+      return Promise.resolve(null);
     }
+
+    stripePromise = loadStripe(publishableKey);
   }
+
   return stripePromise;
 }
