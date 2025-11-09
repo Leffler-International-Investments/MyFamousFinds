71b5f32a07ec44f6899e8f234beb1bad58ee4512
Dan Leffler <leffleryd@gmail.com>
2025-11-10 10:09:31 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
+// FILE: tailwind.config.js
+// This file is REQUIRED to make the Vercel build pass.
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // This tells Tailwind to scan all your pages and components
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 };
