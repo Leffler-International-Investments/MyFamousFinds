ee4e574c389990c0ba10416bd6acb1f6318013dc
Dan Leffler <leffleryd@gmail.com>
2025-11-10 09:39:09 +1100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
 // FILE: postcss.config.js
+// VERCEL CACHE-BUSTING CHANGE v2
 //
-// The build is failing because this file is wrong.
-//
-// Your package.json uses Tailwind v4, which REQUIRES
-// the plugin to be '@tailwindcss/postcss'.
-//
-// This is the only file you need to replace.
+// This file is correct. The build is failing from a cache.
 //
 module.exports = {
   plugins: {
