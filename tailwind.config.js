1b764d7877f13fdbb9d31f5d3fbe358f91038095
Dan Leffler <leffleryd@gmail.com>
2025-11-10 08:52:28 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // This is the most important part.
-    // It tells Tailwind to scan all these files for classes.
+    // This tells Tailwind to scan all your pages and components
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
