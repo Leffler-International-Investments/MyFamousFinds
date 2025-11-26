dce9fb54ed00be2b830a8d10abe2f2b1c0042d24
Dan Leffler <leffleryd@gmail.com>
2025-11-26 17:24:09 +1100

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="sitemap"
+            type="application/xml"
+            href="/sitemap.xml"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
